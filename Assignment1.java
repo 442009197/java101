d5e0c997e48c52868e237233965602e2ce795226
Thamerabdullah <91347051+Thamerabdullah@users.noreply.github.com>
2021-09-24 23:06:03 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,36 @@
+
+//Thamer Abdullah Almahabi
+//442009197
+
+import java.util.Scanner;
+
+public class HW {
+
+   public static void main(String[] args) {
+        
+       
+        Scanner in = new Scanner(System.in);
+
+ 
+        System.out.println("Enter the first float number :");
+        double n1 = in.nextDouble();
+        System.out.println("Enter second float number :");
+        double n2 = in.nextDouble();
+        
+
+
+        
+System.out.println(n1+" + "+n2+" = "+(n1+n2));
+System.out.println(n1+" - "+n2+" = "+(n1-n2));
+System.out.println(n1+" * "+n2+" = "+(n1*n2));
+System.out.println(n1+" ÷ "+n2+" = "+(n1/n2));
+System.out.println(n1+" % "+n2+" = "+(n1%n2));
+System.out.println(n1+" > "+n2+" = "+(n1>n2));
+System.out.println(n1+" >= "+n2+" = "+(n1>=n2));
+System.out.println(n1+" < "+n2+" = "+(n1<n2));
+System.out.println(n1+" <= "+n2+" = "+(n1<=n2));
+System.out.println(n1+" == "+n2+" = "+(n1==n2));
+System.out.println(n1+" != "+n2+" = "+(n1!=n2));
+        
+    }
+}
